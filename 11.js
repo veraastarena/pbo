c1dd1234b399cf4d483ea50437b4d06e960c8f41
veraastarena <veraastarena180@gmail.com>
2025-11-20 19:41:20 +0700

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,17 @@
+ let mahasiswa = new Map();
+ mahasiswa.set("001", "Andi");
+ mahasiswa.set("002", "Budi");
+ mahasiswa.set("003", "Vera");
+ mahasiswa.set("004", "Putra");
+ mahasiswa.set("002", "Vera");
+ 
+ console.log(mahasiswa.get("001")); // Output: Andi
+ console.log(mahasiswa.get("002"));
+ console.log(mahasiswa.get("003"));
+ console.log(mahasiswa.get("004"));
+ 
+  let mahasiswi = ["Andi", "Budi", "Citra", "Dewi"];
+ console.log(mahasiswi[0]); // Output: Andi
+ 
+  let angka = new Set([1, 2, 3, 3, 4, 5, 5]);
+ console.log(angka); // Output: {1, 2, 3, 4, 5}
